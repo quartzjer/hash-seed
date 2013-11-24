f5c48fa6c21455daac7806250f5c2a62d29f75ce
David Van Duzer <dvd@tennica.net>
2013-11-24 13:54:18 -0700

    support for hints file to reconnect to known seeds in a mesh

--- seed.js
+++ seed.js
@@ -3,6 +3,7 @@ var tele = require("telehash");
 var argv = require("optimist")
   .default("id", "./seed.json")
   .default("port", 42424)
+  .default("mesh", "./hints.json")
   .boolean("v").describe("v", "verbose")
   .argv;
 
@@ -10,19 +11,32 @@ if(argv.v) tele.debug(console.log);
 
 var port = parseInt(argv.port);
 
+// optionally use a hints file to accelerate mesh building
+var mesh = [];
+if(fs.existsSync(argv.mesh))
+{
+  mesh = require(argv.mesh);
+}
+
 // load the pub/private key or create one
 if(fs.existsSync(argv.id))
 {
-  init(require(argv.id));
+  init(require(argv.id), mesh);
 }else{
   tele.genkey(function(err, key){
     fs.writeFileSync(argv.id, JSON.stringify(key, null, 4));
-    init(key);
+    init(key, mesh);
   });
 }
 
-function init(key)
+function init(key, mesh)
 {
   var seed = tele.hashname(key, {port:port});
   console.log(JSON.stringify({ip:seed.ip, port:seed.port, hashname:seed.hashname, pubkey:key.public}, null, 4));
+  mesh.forEach(seed.addSeed, seed);
+  seed.online(function(err){
+    console.log((err?err:"connected to mesh seed peers"));
+    if(err) process.exit(0);
+  });
+
 }
