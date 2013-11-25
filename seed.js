6cfd8d695bbeee76cf8e8998a4bf6eaa8cfc073d
Jeremie Miller <jeremie@jabber.org>
2013-11-25 01:52:57 -0700

    renaming hints to seeds to be consistent w/ elsewhere and use hints in the future elsewise

--- seed.js
+++ seed.js
@@ -3,7 +3,6 @@ var tele = require("telehash");
 var argv = require("optimist")
   .default("id", "./seed.json")
   .default("port", 42424)
-  .default("mesh", "./hints.json")
   .boolean("v").describe("v", "verbose")
   .argv;
 
@@ -11,17 +10,10 @@ if(argv.v) tele.debug(console.log);
 
 var port = parseInt(argv.port);
 
-// optionally use a hints file to accelerate mesh building
-var mesh = [];
-if(fs.existsSync(argv.mesh))
-{
-  mesh = require(argv.mesh);
-}
-
 // load the pub/private key or create one
 if(fs.existsSync(argv.id))
 {
-  init(require(argv.id), mesh);
+  init(require(argv.id));
 }else{
   tele.genkey(function(err, key){
     fs.writeFileSync(argv.id, JSON.stringify(key, null, 4));
@@ -29,14 +21,17 @@ if(fs.existsSync(argv.id))
   });
 }
 
-function init(key, mesh)
+function init(key)
 {
   var seed = tele.hashname(key, {port:port});
   console.log(JSON.stringify({ip:seed.ip, port:seed.port, hashname:seed.hashname, pubkey:key.public}, null, 4));
-  mesh.forEach(seed.addSeed, seed);
-  seed.online(function(err){
-    console.log((err?err:"connected to mesh seed peers"));
-    if(err) process.exit(0);
-  });
-
+  // optionally use a seeds file to accelerate mesh building
+  if(argv.seeds)
+  {
+    require(argv.seeds).forEach(seed.addSeed, seed);
+    seed.online(function(err){
+      console.log((err?err:"connected to mesh seed peers"));
+      if(err) process.exit(0);
+    });
+  }
 }
